957d01daf119f47a257fcd4fd14574cf97d778eb
Pietro Maietta <155844333+pietroscik@users.noreply.github.com>
2025-09-27 19:28:09 +0200

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,21 @@
-<nav>
-  <a href="/">Home</a>
-  <a href="/blog">Blog</a>
-  <a href="/sponsor">Collabora</a>
-  <a href="/staking">Calcolatore Staking</a>
-</nav>
+export default function Layout({ children }) {
+  return (
+    <>
+      <header>
+        <h1>Crypto-Attuario</h1>
+        <nav>
+          <a href="/">Home</a>
+          <a href="/blog">Blog</a>
+          <a href="/sponsor">Collabora</a>
+          <a href="/staking">Calcolatore Staking</a>
+        </nav>
+      </header>
+
+      <main>{children}</main>
+
+      <footer>
+        <p>© 2025 Crypto-Attuario</p>
+      </footer>
+    </>
+  );
+}
