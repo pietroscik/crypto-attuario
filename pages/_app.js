45fbe70d4275205f6c856c8e8fc2894e33d4ba2f
Pietro Maietta <155844333+pietroscik@users.noreply.github.com>
2025-09-28 11:25:34 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,17 @@
-import '../styles/style.css'
+import "../styles/style.css";
+import Script from "next/script";
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      {/* Script AdSense, valido per tutto il sito */}
+      <Script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8531177897035530"
+        crossOrigin="anonymous"
+        strategy="afterInteractive"
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
